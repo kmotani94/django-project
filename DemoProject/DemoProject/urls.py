09747170acb776db9d7972d5d104d1ea3eed9640
Karan Motani <karan.motani94@gmail.com>
2019-08-05 18:33:07 -0500

    dynamic binding of views in an app to the project

--- DemoProject/DemoProject/urls.py
+++ DemoProject/DemoProject/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from home.views import home
+from django.urls import path, include
+import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name="Home")
+    path('', include('home.urls')),
 ]
--- DemoProject/home/views.py
+++ DemoProject/home/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
     return HttpResponse("HOME PAGE")
+
+
+def about(request):
+    return HttpResponse("ABOUT PAGE")
+
+
+def gallery(response):
+    return HttpResponse("GALLERY PAGE")
