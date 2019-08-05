3a8236237830a3e50e8c16958e00a5849813aeb1
Karan Motani <karan.motani94@gmail.com>
2019-08-05 16:44:15 -0500

    create django project

---
+++ ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/karanmotani/Desktop/Assignments/django-project/ENV/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
