71d96ba682e576aeaefda93133623fa8b376f7af
Karan Motani <karan.motani94@gmail.com>
2019-08-08 16:31:14 -0500

    add model User

--- DemoProject/home/models.py
+++ DemoProject/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True, primary_key=True)
+    fullname = models.CharField(max_length=50, unique=False)
+    age = models.IntegerField()
+    password = models.CharField(max_length=50, unique=False)
